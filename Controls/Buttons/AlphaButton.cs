33937617a05d48a2c696a4406c2b92ce6391d0fe
iswenzz <suxlolz1528@gmail.com>
2019-04-17 20:33:30 +0200

    AlphaButton: class that inherit Button and supports alpha

---
+++ Controls/Buttons/AlphaButton.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Iswenzz.UI.Controls.Buttons
+{
+    public partial class AlphaButton : Button
+    {
+        public AlphaButton()
+        {
+            InitializeComponent();
+            SetStyle(ControlStyles.SupportsTransparentBackColor, true);
+            SetStyle(ControlStyles.Opaque, true);
+        }
+
+        protected override void OnParentBackColorChanged(EventArgs e)
+        {
+            Invalidate();
+            base.OnParentBackColorChanged(e);
+        }
+
+        protected override void OnBackColorChanged(EventArgs e)
+        {
+            if (Parent != null)
+                Parent.Invalidate(Bounds, true);
+            base.OnBackColorChanged(e);
+        }
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams cp = base.CreateParams;
+                cp.ExStyle = cp.ExStyle | 0x20;
+                return cp;
+            }
+        }
+    }
+}
