96db65e335327abbb29bfbb49bc6af304cf8d1d6
iswenzz <suxlolz1528@gmail.com>
2019-04-17 21:48:04 +0200

    SeparatorPanel: panel w/ a separator at the top & bottom, + color & thickness

---
+++ Controls/Containers/SeparatorPanel.cs
@@ -0,0 +1,42 @@
+﻿using System.Drawing;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Iswenzz.UI.Controls.Containers
+{
+    public partial class SeparatorPanel : Panel
+    {
+        private Color borderColor;
+        [Description("Change the border color.")]
+        public Color BorderColor
+        {
+            get => borderColor;
+            set { borderColor = value; Invalidate(); }
+        }
+
+        private float thickness;
+        [DefaultValue(1f)]
+        [Description("Change the border thickness.")]
+        public float Thickness
+        {
+            get => thickness;
+            set { thickness = value; Invalidate(); }
+        }
+
+        public SeparatorPanel()
+        {
+            InitializeComponent();
+            BorderColor = Color.DimGray;
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+
+            pe.Graphics.DrawLine(new Pen(BorderColor, Thickness),
+                new Point(0, 0), new Point(Width, 0));
+            pe.Graphics.DrawLine(new Pen(BorderColor, Thickness), 
+                new Point(0, Height - 1), new Point(Width, Height - 1));
+        }
+    }
+}
