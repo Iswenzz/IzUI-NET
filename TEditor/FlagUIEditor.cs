216b14b3054f6f1cc9eafd0a4e88908791a96e4b
iswenzz <suxlolz1528@gmail.com>
2019-04-18 02:41:12 +0200

    FlagUIEditor: UITypeEditor for FlagCheckedListBox

---
+++ TEditor/FlagUIEditor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Windows.Forms.Design;
+
+using Iswenzz.UI.Controls;
+
+namespace Iswenzz.UI.TEditor
+{
+    public class FlagUIEditor : UITypeEditor
+    {
+        private FlagCheckedListBox flagListBox;
+
+        public FlagUIEditor()
+        {
+            flagListBox = new FlagCheckedListBox();
+        }
+
+        public override object EditValue(ITypeDescriptorContext context, IServiceProvider provider, object value)
+        {
+            if (context != null && context.Instance != null && provider != null)
+            {
+                IWindowsFormsEditorService edSvc = (IWindowsFormsEditorService)provider.GetService(typeof(IWindowsFormsEditorService));
+
+                if (edSvc != null)
+                {
+                    Enum e = (Enum)Convert.ChangeType(value, context.PropertyDescriptor.PropertyType);
+                    flagListBox.EnumValue = e;
+                    edSvc.DropDownControl(flagListBox);
+                    return flagListBox.EnumValue;
+                }
+            }
+            return null;
+        }
+
+        public override UITypeEditorEditStyle GetEditStyle(ITypeDescriptorContext context)
+        {
+            return UITypeEditorEditStyle.DropDown;
+        }
+    }
+}
