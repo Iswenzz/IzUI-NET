e0c9e16615f0d38f8fe15ce2f9bae90c49598850
Iswenzz <suxlolz1528@gmail.com>
2021-11-07 15:52:15 +0100

    Renamed project to IzUI-NET

--- Properties/Resources.Designer.cs
+++ Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Iswenzz.UI.Properties {
+namespace Iswenzz.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace Iswenzz.UI.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Iswenzz.UI.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Iswenzz.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
